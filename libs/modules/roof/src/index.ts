60adfd09b2318ce4d047550e480a002c5d657078
Yury Vakolov <y.vakolov@juralab.ru>
2026-09-10 18:19:03 +0300

    feat: add roof module manifest

---
+++ libs/modules/roof/src/index.ts
@@ -0,0 +1,9 @@
+import type { SmartBarnModuleManifest } from '@smartbarn/platform-core';
+
+export const ROOF_MODULE: SmartBarnModuleManifest = {
+  id: 'roof',
+  name: 'Roof',
+  version: '0.1.0',
+  entityTypes: ['roof'],
+  capabilities: [],
+};
