f7dfd1f52ff52d102e2c8b329183b7142f7580f9
Yury Vakolov <y.vakolov@juralab.ru>
2026-09-10 21:24:03 +0300

    feat(platform): export command bus contracts

--- libs/platform/core/src/index.ts
+++ libs/platform/core/src/index.ts
@@ -1,2 +1,5 @@
 export * from './lib/contracts';
 export * from './lib/cqrs/messageToken';
+export * from './lib/cqrs/command';
+export * from './lib/cqrs/commandHandlerRegistry';
+export * from './lib/cqrs/commandBus';
