a261a56b72ab8655eb773be05477e4d118a78467
Yury Vakolov <y.vakolov@juralab.ru>
2026-09-10 18:18:46 +0300

    feat: add walls module manifest

---
+++ libs/modules/walls/src/index.ts
@@ -0,0 +1,9 @@
+import type { SmartBarnModuleManifest } from '@smartbarn/platform-core';
+
+export const WALLS_MODULE: SmartBarnModuleManifest = {
+  id: 'walls',
+  name: 'Walls',
+  version: '0.1.0',
+  entityTypes: ['wall'],
+  capabilities: [],
+};
