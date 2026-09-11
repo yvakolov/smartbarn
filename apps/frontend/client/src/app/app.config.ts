e07cefb7f2d5bbb9513e08d56dc9ee790bc6a14d
Yury Vakolov <y.vakolov@juralab.ru>
2026-09-11 11:44:45 +0300

    fix(pages): use hash routing for static hosting

--- apps/frontend/client/src/app/app.config.ts
+++ apps/frontend/client/src/app/app.config.ts
@@ -1,13 +1,13 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withHashLocation } from '@angular/router';
 import { SMARTBARN_I18N_PROVIDERS } from '@smartbarn/platform-i18n';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
-    provideRouter(appRoutes, withComponentInputBinding()),
+    provideRouter(appRoutes, withComponentInputBinding(), withHashLocation()),
     ...SMARTBARN_I18N_PROVIDERS,
   ],
 };
