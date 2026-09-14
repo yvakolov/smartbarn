07d6de12e8c40e57975bebf1a9d4636d95664d13
Yury Vakolov <y.vakolov@juralab.ru>
2026-09-14 16:26:06 +0300

    feat(ui): scaffold Bootstrap icon collection

---
+++ libs/ui/icons/src/lib/bootstrap.ts
@@ -0,0 +1,13 @@
+import type { IconDefinition } from './icon.types';
+
+/** Bootstrap Icons search icon. */
+export const bootstrapSearch: IconDefinition = {
+  collection: 'bootstrap',
+  name: 'search',
+  viewBox: '0 0 16 16',
+  paths: [
+    'M11 6a5 5 0 1 1-10 0 5 5 0 0 1 10 0Zm-1.293 4.707a6 6 0 1 1 1.414-1.414l3.147 3.146a1 1 0 0 1-1.414 1.415l-3.147-3.147Z',
+  ],
+  fill: 'currentColor',
+  stroke: 'none',
+};
