e3fdc16673abec82ae0f99e11d8d0a70fb6b76c2
Yury Vakolov <y.vakolov@juralab.ru>
2026-09-14 16:26:53 +0300

    feat(ui): export icon primitive

--- libs/ui/kit/src/index.ts
+++ libs/ui/kit/src/index.ts
@@ -3,6 +3,7 @@
 // from this public API. Application code should consume SmartBarn primitives
 // through @smartbarn/ui-kit instead of coupling feature code to implementation details.
 
+export * from './lib/icon.component';
 export * from './lib/search-combobox.component';
 
 export const SMARTBARN_UI_KIT = 'smartbarn-ui-kit';
