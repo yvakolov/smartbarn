3e8db44f3531a47b3e9c976ee21bfe3b7153602f
Yury Vakolov <y.vakolov@juralab.ru>
2026-09-15 21:51:13 +0300

    Add persistent full profile view

--- libs/ui/kit/src/index.ts
+++ libs/ui/kit/src/index.ts
@@ -2,4 +2,5 @@ export * from './lib/icon.component';
 export * from './lib/search-combobox.component';
 export * from './lib/input-text.component';
 export * from './lib/input-number.component';
+export * from './lib/checkbox.component';
 export const JURALAB_UI_KIT='juralab-ui-kit';
