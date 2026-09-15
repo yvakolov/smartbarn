b984728ed62d5ca890a2114e3fc8aed7da57e5c4
Yury Vakolov <y.vakolov@juralab.ru>
2026-09-15 19:39:06 +0300

    Refine CAD profile and standardize UI kit controls

--- libs/ui/kit/src/index.ts
+++ libs/ui/kit/src/index.ts
@@ -1,9 +1,5 @@
-// Smart Barn shared UI kit.
-// Spartan-style reusable UI primitives live in this library and are re-exported
-// from this public API. Application code should consume SmartBarn primitives
-// through @smartbarn/ui-kit instead of coupling feature code to implementation details.
-
 export * from './lib/icon.component';
 export * from './lib/search-combobox.component';
-
-export const SMARTBARN_UI_KIT = 'smartbarn-ui-kit';
+export * from './lib/input-text.component';
+export * from './lib/input-number.component';
+export const JURALAB_UI_KIT='juralab-ui-kit';
